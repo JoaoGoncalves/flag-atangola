7edb7e83025303c9c50d080873381410e79d5103
Joao Goncalves <joao@joaogoncalves.net>
2025-10-20 10:50:20 +0100

    formgroup e fom html

--- hrms/src/app/pages/employees/edit-employee/edit-employee.ts
+++ hrms/src/app/pages/employees/edit-employee/edit-employee.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { PermissionsService } from '../../../services/permission-service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { EmployeeForm } from '../../../infrastructure/types/employee-form';
 
 @Component({
   selector: 'app-edit-employee',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './edit-employee.html',
-  styleUrl: './edit-employee.css'
+  styleUrl: './edit-employee.css',
 })
 export class EditEmployee {
+  permission = inject(PermissionsService);
 
+  form = new FormGroup<EmployeeForm>({
+    firstName: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+    lastName: new FormControl('', { nonNullable: true }),
+    email: new FormControl('', { nonNullable: true }),
+    position: new FormControl('', { nonNullable: true }),
+    level: new FormControl('', { nonNullable: true }),
+  });
 }
